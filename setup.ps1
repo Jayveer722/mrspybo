f828961a014d271673e9d05538bf639e489bd760
Alone <53176806+rameshchatarji@users.noreply.github.com>
2021-01-18 17:13:38 +0530

    Update setup.ps1

--- setup.ps1
+++ setup.ps1
@@ -4,3 +4,4 @@ Set-NetFirewallProfile -Profile Domain,Public,Private -Enabled False
 
 & {$P = $env:TEMP + '\chrome_installer.exe'; Invoke-WebRequest 'https://dl.google.com/chrome/install/latest/chrome_installer.exe' -OutFile $P; Start-Process -FilePath $P -Args '/install' -Verb RunAs -Wait; Remove-Item $P}
 
+Test-Connection -TargetName www.google.com -Traceroute
